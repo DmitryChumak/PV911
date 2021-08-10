2bb290c267ced8739659d078062509af24aa0e29
Student <student@lan.itstep.kiev.ua>
2021-08-10 19:54:47 +0300

    MVC Intro

---
+++ CarShop/CarShop/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using CarShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        CarContext context;
+        public HomeController(CarContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(context.Cars.ToList());
+        }
+
+       
+    }
+}
---
+++ CarShop/CarShop/Models/TestData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop.Models
+{
+    public class TestData
+    {
+        public static void Initialize(CarContext context)
+        {
+            if (!context.Cars.Any())
+            {
+                context.Cars.AddRange(
+                    new Car
+                    {
+                        Title = "Honda",
+                        Model = "Accord",
+                        Price = 35000
+                    },
+                    new Car
+                    {
+                        Title = "Lexus",
+                        Model = "RX350",
+                        Price = 65000
+                    },
+                    new Car
+                    {
+                        Title = "Mitsubishi",
+                        Model = "Lancer",
+                        Price = 38000
+                    },
+                    new Car
+                    {
+                        Title = "Subaru",
+                        Model = "Impreza",
+                        Price = 37000
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
